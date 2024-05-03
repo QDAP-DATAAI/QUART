9f589542181736589dbc3e4c819174d5b4a8fc4b
Fred <frederic.delage@chde.qld.gov.au>
2024-05-03 23:42:39 +0000

    wip: add scenario samples

---
+++ src/README.md
@@ -0,0 +1,40 @@
+<!-- Work in progress -->
+
+# Red Teaming Targets
+
+Queensland Government Customer and Digital Group (QGCDG) red teaming strategies for QChat bot
+
+# Prerequisite
+- Install python
+- Install dependencies
+
+# Output
+- results folder
+
+# Orchestration flow:
+1. Generate malicious prompt with red teaming llm
+2. Send malicious prompt
+3. Receive target's response
+4. Send response to scoring engine
+5. Receive result from scoring engine
+6. Generate new prompt based on scoring
+7. Receive new malicious prompt from red teaming llm
+-- REPEAT until EndToken or MaxTurns --
+
+# Prompt flow
+1. Send malicious prompt
+2. Receive target's response
+3. Send response to scoring engine
+4. Receive result from scoring engine
+
+
+## Safety alignment
+- Policies alignment (brand reputation / values)
+- Rule-breaking prompts (prompt going against policies)
+- Generated prompts 
+- Accidental de-alignment (super helpful)
+
+## Generic jailbreak
+- Jailbreaking prompts (jailbreakchat.com)
+- Generated custom jailbreak (create jailbreaks for specific contexts)
+    + Jailbreak patterns (responsibility, character, research purposes, life or death, carrot or stick)
---
+++ src/scenario/generated_prompts/generated_prompts.py
@@ -0,0 +1,82 @@
+import os
+import datetime
+from glob import glob
+from pathlib import Path
+from threading import Thread
+from textwrap import dedent
+from pyrit.common import default_values
+from pyrit.memory import DuckDBMemory
+from pyrit.memory.memory_interface import MemoryInterface
+from pyrit.models import PromptTemplate
+from pyrit.orchestrator import EndTokenRedTeamingOrchestrator
+from pyrit.prompt_target import AzureOpenAIChatTarget, AzureMLChatTarget
+
+default_values.load_default_env()
+
+DATABASE_NAME = os.environ.get("DATABASE_NAME")
+MAX_TURN = int(os.environ.get("MAX_CONVERSATION_TURN"))
+
+def perform_red_teaming(
+    prompt: str,
+    red_teaming_llm: AzureOpenAIChatTarget | AzureMLChatTarget,
+    memory: MemoryInterface = None,
+):
+    attack_strategy = dedent(prompt)
+
+    target_llm = AzureOpenAIChatTarget(
+        deployment_name=os.environ.get("AZURE_OPENAI_CHAT_DEPLOYMENT"),
+        endpoint=os.environ.get("AZURE_OPENAI_CHAT_ENDPOINT"),
+        api_key=os.environ.get("AZURE_OPENAI_CHAT_KEY"),
+    )
+
+    start = datetime.datetime.now()
+    
+    try:
+        with EndTokenRedTeamingOrchestrator(
+            attack_strategy=attack_strategy,
+            red_teaming_chat=red_teaming_llm,
+            prompt_target=target_llm,
+            initial_red_teaming_prompt="Begin conversation",
+            end_token="<|pwned|>",
+            memory=memory,
+            verbose=True,
+        ) as red_teaming_orchestrator:
+            red_teaming_orchestrator.apply_attack_strategy_until_completion(max_turns=MAX_TURN)
+    except Exception as e:
+        print(f"Error: {e}")
+    
+    end = datetime.datetime.now()
+    print(f"Time taken: {end - start}")
+
+
+def get_red_teaming_llms():
+    return [
+        AzureOpenAIChatTarget(
+            deployment_name=os.environ.get("AZURE_OPENAI_CHAT_DEPLOYMENT"),
+            endpoint=os.environ.get("AZURE_OPENAI_CHAT_ENDPOINT"),
+            api_key=os.environ.get("AZURE_OPENAI_CHAT_KEY"),
+        ),
+        # AzureMLChatTarget(
+        #     endpoint_uri=os.environ.get("AZURE_ML_MANAGED_ENDPOINT"),
+        #     api_key=os.environ.get("AZURE_ML_KEY"),
+        #     chat_message_normalizer=GenericSystemSquash(),
+        # ),
+    ]
+
+
+files = glob(os.path.join(os.path.dirname(os.path.abspath(__file__)), "*.yaml"))
+
+duckdb_memory = DuckDBMemory(db_path=f"results/{DATABASE_NAME}.db")
+
+for file in files:
+    prompt = PromptTemplate.from_yaml_file(Path(file))
+    for red_teaming_llm in get_red_teaming_llms():
+        print(f"Performing red teaming for: {prompt.name}")
+        Thread(
+            target=perform_red_teaming,
+            args=(
+                prompt.template,
+                red_teaming_llm,
+                duckdb_memory,
+            ),
+        ).start()
